a0feea25baaeb3a815b25471e74e5274ce5a2975
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-23 14:46:29 -0500

    msm8909-common: Cleanup dead targets
    Offending entries:
    android.hardware.renderscript@1.0-service
    libgenlock
    libmm-qcamera
    libwcnss_qmi
    build/make/core/main.mk:1146: error: Build failed.
    
    To do this yourself, put the following line into
    lineage_$TARGET.mk:
    
    `$(call enforce-product-packages-exist,)`
    
    Note that the above flag must be in the product
    makefile to work.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- msm8909.mk
+++ msm8909.mk
@@ -75,7 +75,6 @@ TARGET_BOOTANIMATION_HALF_RES := true
 PRODUCT_PACKAGES += \
     gralloc.msm8909 \
     hwcomposer.msm8909 \
-    libgenlock \
     libtinyxml \
     memtrack.msm8909
 
@@ -206,8 +205,7 @@ PRODUCT_PACKAGES += \
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl \
-    android.hardware.renderscript@1.0-service
+    android.hardware.renderscript@1.0-impl
 
 # Seccomp
 PRODUCT_COPY_FILES += \
@@ -237,7 +235,6 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    libwcnss_qmi \
     wcnss_service
 
 PRODUCT_PACKAGES += \
