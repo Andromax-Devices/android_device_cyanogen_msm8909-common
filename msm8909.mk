0635b8c88f6c46700d6c04ebee61fd7249fd6a6e
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-03-05 06:15:10 +0700

    Convert msm8916-common to msm8909-common

--- msm8909.mk
+++ msm8909.mk
@@ -37,7 +37,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    audio.primary.msm8916 \
+    audio.primary.msm8909 \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
@@ -63,11 +63,11 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 # Display
 PRODUCT_PACKAGES += \
-    gralloc.msm8916 \
-    hwcomposer.msm8916 \
+    gralloc.msm8909 \
+    hwcomposer.msm8909 \
     libgenlock \
     libtinyxml \
-    memtrack.msm8916
+    memtrack.msm8909
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -120,12 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh
 
-# IRQ balance
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-endif
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
@@ -133,7 +127,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
 PRODUCT_PACKAGES += \
-    keystore.msm8916
+    keystore.msm8909
 endif
 
 PRODUCT_PACKAGES += \
@@ -181,11 +175,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
-endif
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
@@ -210,12 +199,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # System Properties
-$(call inherit-product, device/cyanogen/msm8916-common/system_prop.mk)
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-$(call inherit-product, device/cyanogen/msm8916-common/system_prop_msm8939.mk)
-else ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
-$(call inherit-product, device/cyanogen/msm8916-common/system_prop_msm8916.mk)
-endif
+$(call inherit-product, device/cyanogen/msm8909-common/system_prop_msm8909.mk)
 
 # TimeKeep
 PRODUCT_PACKAGES += \
